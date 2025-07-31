204fd7db8580f7c79bef9975203f297840c232ab
Armanul Momin <armanul.momin@gmail.com>
2025-07-31 20:03:43 +1000

    Shapes module

---
+++ SwiftUIMastery/ShapesBootcamp.swift
@@ -0,0 +1,28 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUIMastery
+//
+//  Created by Arman on 31/7/25.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+       // Circle()
+        //Ellipse()
+       // Rectangle()
+       // RoundedRectangle(cornerRadius: 20)
+        Capsule()
+           // .fill(Color.blue)
+            //.foregroundStyle(Color.red)
+            //.stroke(Color.red, lineWidth: 10)
+            //.stroke(Color.blue, style: StrokeStyle(lineWidth: 30, lineCap: .round,dash: [30, 50]))
+            //.trim(from: 0.2,to: 1.0)
+        
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
