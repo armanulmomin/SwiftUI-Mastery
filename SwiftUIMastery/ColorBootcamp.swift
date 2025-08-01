886b06ec0b9c060fc216b750b8b4b16f86124d35
Armanul Momin <armanul.momin@gmail.com>
2025-08-01 18:08:22 +1000

    Color Module

---
+++ SwiftUIMastery/ColorBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  ColorBootcamp.swift
+//  SwiftUIMastery
+//
+//  Created by Arman on 1/8/25.
+//
+
+import SwiftUI
+
+struct ColorBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+           // .fill(Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)))
+            .fill(Color("CustomColor"))
+            .frame(width: 250,height: 250)
+    }
+}
+
+#Preview {
+    ColorBootcamp()
+}
