2e16d94989a6e98d3d3ea9aebbbfae90d9bdda33
Armanul Momin <armanul.momin@gmail.com>
2025-08-17 17:21:10 +1000

    Working on static text

---
+++ SwiftUIMastery/StaticTextBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  StaticTextBootcamp.swift
+//  SwiftUIMastery
+//
+//  Created by Arman on 17/8/25.
+//
+
+import SwiftUI
+
+struct StaticTextBootcamp: View {
+    var alignments: [TextAlignment] = [.leading, .trailing, .center]
+    @State private var alignment = TextAlignment.leading
+    var body: some View {
+        /*
+       // creating static labels with a text view
+        Text("This is a longer text view, which needs couple of lines and i am going to use line limit to figure it out what i can do it while keeping the view compact with the help of lineLimit along with using frame.")
+            .lineLimit(2, reservesSpace: true)
+            .frame(width: 200)
+            .truncationMode(.middle)
+       
+         */
+        
+        /*
+       // How to style text views with fonts, colors, line spacing and more
+        Text("This is a longer text view, which needs couple of lines and i am going to use line limit to figure it out what i can do it while keeping the view compact with the help of lineLimit along with using frame.")
+        
+            .fontWidth(.compressed)
+            .frame(width: 300)
+            .lineSpacing(50)
+            .foregroundStyle(.red.gradient)
+            .background(Color.blue)
+         */
+        
+        // How to adjust text alignment using multiline text Allignment()
+        VStack{
+            Picker("Text alignment", selection: $alignment){
+                ForEach(alignments, id: \.self){ alignment in
+                    //Text(String(describing: alignment))
+                    Text("\(alignment)")
+                    
+                }
+                
+            }
+            Text("This is a longer text view, which needs couple of lines and i am going to use line limit to figure it out what i can do it while keeping the view compact with the help of lineLimit along with using frame.")
+                .font(.largeTitle)
+                .multilineTextAlignment(alignment)
+                .frame(width: 300)
+        }
+        //How to format text inside text views
+    }
+}
+
+#Preview {
+    StaticTextBootcamp()
+}
