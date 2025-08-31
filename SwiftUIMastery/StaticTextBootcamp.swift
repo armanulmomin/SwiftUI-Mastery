1d74ce9e13a490bafd14d3014e79941fd79e640d
Armanul Momin <armanul.momin@gmail.com>
2025-08-31 18:47:07 +1000

    Static text Module Modified

--- SwiftUIMastery/StaticTextBootcamp.swift
+++ SwiftUIMastery/StaticTextBootcamp.swift
@@ -11,14 +11,14 @@ struct StaticTextBootcamp: View {
     var alignments: [TextAlignment] = [.leading, .trailing, .center]
     @State private var alignment = TextAlignment.leading
     var body: some View {
-        /*
+        
        // creating static labels with a text view
         Text("This is a longer text view, which needs couple of lines and i am going to use line limit to figure it out what i can do it while keeping the view compact with the help of lineLimit along with using frame.")
             .lineLimit(2, reservesSpace: true)
             .frame(width: 200)
             .truncationMode(.middle)
        
-         */
+         
         
         /*
        // How to style text views with fonts, colors, line spacing and more
