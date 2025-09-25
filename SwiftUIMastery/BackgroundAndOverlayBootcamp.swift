eb47c6bd15b6c0586f0bde71aa440283a41e6a65
Armanul Momin <armanul.momin@gmail.com>
2025-09-25 21:23:13 +1000

    Background modified

--- SwiftUIMastery/BackgroundAndOverlayBootcamp.swift
+++ SwiftUIMastery/BackgroundAndOverlayBootcamp.swift
@@ -9,33 +9,43 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .background(Color.red)
-            .overlay(
-                Circle()
-                    .fill(Color.gray).opacity(0.5)
-                
-            )
-        Spacer()
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .background(Color.red)
-            .overlay(
-                Circle()
-                    .fill(Color.gray).opacity(0.5)
-                
-            )
-        Spacer()
-        
-        Text("Custom Overlay")
-                        .font(.title2)
-                        .padding()
-                        .background(Color.green)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(Color.black, lineWidth: 3)
-                                .padding(5)
-                        )
+        VStack(spacing: 20) {
+            
+            Text("Hello, World!")
+                .padding()
+                .background(Color.red)
+                .overlay(
+                    Circle()
+                        .fill(Color.gray)
+                        .opacity(0.5)
+                )
+            
+            Spacer()
+            
+            Text("Hello, World!")
+                .padding()
+                .background(Color.red)
+                .overlay(
+                    Circle()
+                        .fill(Color.gray)
+                        .opacity(0.5)
+                )
+            
+            Spacer()
+            
+            Text("Custom Overlay")
+                .font(.title2)
+                .padding()
+                .background(Color.green)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.black, lineWidth: 3)
+                        .padding(5)
+                )
+        }
+        .padding()
     }
+
 }
 
 #Preview {
