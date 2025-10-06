64882ea50f1eb03678dbfbfc8d09cb11dd2649b8
Armanul Momin <armanul.momin@gmail.com>
2025-10-06 22:31:16 +1100

    updated

--- SwiftUIMastery/ContentView.swift
+++ SwiftUIMastery/ContentView.swift
@@ -18,10 +18,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
         }
         .padding()
     }
