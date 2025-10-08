68914bfa5e446e901939189d4e1b43bb8a2d61ec
Armanul Momin <armanul.momin@gmail.com>
2025-10-08 23:47:23 +1100

    Image updated

--- SwiftUIMastery/Image Bootcamp.swift
+++ SwiftUIMastery/Image Bootcamp.swift
@@ -27,6 +27,23 @@ struct Image_Bootcamp: View {
                 }
                 .scaleEffect(1.0)
                 .animation(.spring(), value: UUID())
+            Image("Picture")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 200)
+                .clipped()
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color.white, lineWidth: 4)
+                )
+                .shadow(color: .black.opacity(0.3), radius: 10, x: 5, y: 5)
+                .padding()
+                .onTapGesture {
+                    print("Image tapped!")
+                }
+                .scaleEffect(1.0)
+                .animation(.spring(), value: UUID())
         }
 
     }
