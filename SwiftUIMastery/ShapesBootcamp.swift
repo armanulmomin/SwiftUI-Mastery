a681e7f7edf561d2622a677e5ec30fc92932a467
Armanul Momin <armanul.momin@gmail.com>
2025-10-10 11:02:13 +1100

    Shape updated

--- SwiftUIMastery/ShapesBootcamp.swift
+++ SwiftUIMastery/ShapesBootcamp.swift
@@ -31,6 +31,27 @@ struct ShapesBootcamp: View {
                             endAngle: .degrees(270),
                             clockwise: false)
             } // arc
+            Circle()
+            Ellipse()
+            Rectangle()
+            RoundedRectangle(cornerRadius: 20)
+            Capsule()
+            Path { path in
+                path.move(to: CGPoint(x: 20, y: 20))
+                path.addLine(to: CGPoint(x: 100, y: 20))
+                path.addLine(to: CGPoint(x: 60, y: 80))
+                path.closeSubpath()
+            } // custom triangle
+
+            Capsule(style: .continuous)
+            RoundedRectangle(cornerRadius: 50, style: .continuous)
+            Path { path in
+                path.addArc(center: CGPoint(x: 50, y: 50),
+                            radius: 40,
+                            startAngle: .degrees(0),
+                            endAngle: .degrees(270),
+                            clockwise: false)
+            } // arc
         }
         .padding()
         .foregroundStyle(.blue)
