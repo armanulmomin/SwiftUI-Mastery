e9997a8bd11526572c46619208e4924feb5458f6
Armanul Momin <armanul.momin@gmail.com>
2025-11-13 19:41:21 +1100

    string v2

--- SwiftUIMastery/StringPractise.playground/Contents.swift
+++ SwiftUIMastery/StringPractise.playground/Contents.swift
@@ -3,4 +3,10 @@ import UIKit
 var greeting = "Hello, playground"
 print(greeting.isEmpty)
 print(greeting.count)
+print(greeting.isEmpty)
+print(greeting.count)print(greeting.isEmpty)
+print(greeting.count)print(greeting.isEmpty)
+print(greeting.count)print(greeting.isEmpty)
+print(greeting.count)print(greeting.isEmpty)
+print(greeting.count)
 
